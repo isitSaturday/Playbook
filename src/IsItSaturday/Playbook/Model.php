d93a9da1ccf02f498aa32b51bcd6363c4642937e
Dave Widmer <dwidmer@bgsu.edu>
2013-10-04 15:54:36 -0400

    Model class is ready, minus the DB stuff.

--- src/IsItSaturday/Playbook/Model.php
+++ src/IsItSaturday/Playbook/Model.php
@@ -4,47 +4,272 @@ namespace IsItSaturday\Playbook;
 
 class Model implements \ArrayAccess
 {
+	/**
+	 * @var string
+	 */
 	public $idAttribute = 'id';
 
 	protected $data = array();
 	protected $changed = array();
-	protected $default = array();
 	protected $errors = array();
 
-	public function __construct(array $data = array());
+	/**
+	 * @param array $data
+	 */
+	public function __construct(array $data = array())
+	{
+		$this->reset();
 
-	public function get($name, $default = null);
-	public function set($name, $value);
-	public function has($name);
-	public function unset($name);
-	public function clear();
+		if ( ! empty($data))
+		{
+			$this->set($data);
+		}
+	}
 
-	public function id();
-	public function isNew();
+	/**
+	 * @param  string  $name
+	 * @return boolean
+	 */
+	public function has($name)
+	{
+		return \array_key_exists($name, $this->data);
+	}
 
+	/**
+	 * Same as __get() only allowing for a default value if the property isn't found
+	 *
+	 * @see __get()
+	 *
+	 * @param  string $name
+	 * @param  mixed  $default
+	 * @return mixed
+	 */
+	public function get($name, $default = null)
+	{
+		if ( ! $this->has($name))
+		{
+			return $default;
+		}
+
+		return $this->data[$name];
+	}
+
+	/**
+	 * @param  mixed $name   The name of the property or key => value pairs
+	 * @param  mixed $value  If setting a single property, the value of said property
+	 * @return Model
+	 */
+	public function set($name, $value = null)
+	{
+		if ( ! is_array($name))
+		{
+			$name = array($name => $value);
+		}
+
+		foreach ($name as $key => $value)
+		{
+			if ( ! $this->has($key) OR $this->get($key) !== $value)
+			{
+				$this->changed[] = $key;
+			}
+
+			$this->data[$key] = $value;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @param  string $name
+	 * @return mixed  The value of the property before it is removed
+	 */
+	public function remove($name)
+	{
+		$value = $this->get($name);
+		unset($this->data[$name]);
+
+		return $value;
+	}
+
+	/**
+	 * @return Model
+	 */
+	public function reset()
+	{
+		$this->changed = array();
+		$this->errors = array();
+
+		return $this;
+	}
+
+	/**
+	 * @return mixed  The id of the model (or false)
+	 */
+	public function id()
+	{
+		return $this->get($this->idAttribute, false);
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isNew()
+	{
+		return $this->id() === false;
+	}
+
+	/**
+	 * @return string  The name of the table the model is associated with
+	 */
 	public function table()
 	{
 		return '';
 	}
 
-	public function save();
-	public function destroy();
+	public function fetch($id){}
+	public function save(){}
+	public function destroy(){}
 
-	public function jsonSerialize();
-	public function toJson();
+	/**
+	 * Converts the data to json.
+	 *
+	 * @return string
+	 */
+	public function toJSON()
+	{
+		return json_encode($this->data);
+	}
 
-	public function toArray();
-	public function toClass($name);
+	/**
+	 * @see toJSON()
+	 */
+	public function jsonSerialize()
+	{
+		return $this->toJSON();
+	}
 
-	public function validate();
-	public function errors();
-	public function isValid();
+	/**
+	 * @return array
+	 */
+	public function toArray()
+	{
+		return $this->data;
+	}
 
-	public function hasChanged();
-	public function changedAttributes();
+	/**
+	 * @param  string $name
+	 * @return mixed
+	 */
+	public function toClass($name)
+	{
+		return new $name($this->data);
+	}
 
-	public function offsetExists($name);
-	public function offsetGet($name);
-	public function offsetSet($name, $value);
-	public function offsetUnset($name);
+	/**
+	 * @return array
+	 */
+	public function validate()
+	{
+		$this->errors = array();
+		return $this->errors;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function errors()
+	{
+		return $this->errors;
+	}
+
+	/**
+	 * @uses   validate()
+	 * @return boolean
+	 */
+	public function isValid()
+	{
+		$errors = $this->validate();
+		return empty($errors);
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function hasChanged()
+	{
+		return ! empty($this->changed);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function changedAttributes()
+	{
+		return $this->changed;
+	}
+
+	/**
+	 * @see has()
+	 */
+	public function offsetExists($name)
+	{
+		return $this->has($name);
+	}
+
+	/**
+	 * @see get()
+	 */
+	public function offsetGet($name)
+	{
+		return $this->get($name);
+	}
+
+	/**
+	 * @see set()
+	 */
+	public function offsetSet($name, $value)
+	{
+		$this->set($name, $value);
+	}
+
+	/**
+	 * @see remove()
+	 */
+	public function offsetUnset($name)
+	{
+		$this->remove($name);
+	}
+
+	/**
+	 * @see has()
+	 */
+	public function __isset($name)
+	{
+		return $this->has($name);
+	}
+
+	/**
+	 * @see get()
+	 */
+	public function __get($name)
+	{
+		return $this->get($name);
+	}
+
+	/**
+	 * @see set()
+	 */
+	public function __set($name, $value)
+	{
+		$this->set($name, $value);
+	}
+
+	/**
+	 * @see remove()
+	 */
+	public function __unset($name)
+	{
+		$this->remove($name);
+	}
+
 }
---
+++ tests/ModelTest.php
@@ -0,0 +1,112 @@
+<?php
+
+use IsItSaturday\Playbook\Model;
+
+class ModelTest extends PHPUnit_Framework_TestCase
+{
+	public $model;
+
+	public function setUp()
+	{
+		\IsItSaturday\Playbook\Facades\Sync::setup('sqlite::memory:');
+		$this->model = new Model(array('name' => "Dave"));
+	}
+
+	public function testNewModelTracksChanges()
+	{
+		$this->assertSame(array('name'), $this->model->changedAttributes());
+		$this->assertTrue($this->model->hasChanged());
+	}
+
+	public function testMethodLoop()
+	{
+		$this->model->set('job', 'programmer');
+		$this->assertTrue($this->model->has('job'));
+		$this->assertSame('programmer', $this->model->get('job'));
+
+		$this->model->remove('job');
+		$this->assertNull($this->model->get('job'));
+	}
+
+	public function testArrayLoop()
+	{
+		$this->model['job'] = 'programmer';
+		$this->assertTrue($this->model->offsetExists('job'));
+		$this->assertSame('programmer', $this->model['job']);
+
+		$this->model->offsetUnset('job');
+		$this->assertNull($this->model['job']);
+	}
+
+	public function testPropertyLoop()
+	{
+		$this->model->job = 'programmer';
+		$this->assertTrue(isset($this->model->job));
+		$this->assertSame('programmer', $this->model->job);
+
+		unset($this->model->job);
+		$this->assertNull($this->model->job);
+	}
+
+	public function testReset()
+	{
+		$this->model->reset();
+		$this->assertFalse($this->model->hasChanged());
+	}
+
+	public function testNoIdIsFalse()
+	{
+		$this->assertFalse($this->model->id());
+	}
+
+	public function testHasId()
+	{
+		$this->model->set('id', 1);
+		$this->assertSame(1, $this->model->id());
+	}
+
+	public function testDefaultTableIsEmptyString()
+	{
+		$this->assertSame('', $this->model->table());
+	}
+
+	public function testToJSON()
+	{
+		$json = '{"name":"Dave"}';
+		$this->assertSame($json, $this->model->toJSON());
+		$this->assertSame($json, $this->model->jsonSerialize());
+	}
+
+	public function testToArray()
+	{
+		$this->assertSame(array('name' => "Dave"), $this->model->toArray());
+	}
+
+	public function testToClass()
+	{
+		$object = $this->model->toClass("Nametastic");
+		$this->assertSame('evaD', $object->reverseName());
+	}
+
+	public function testValidation()
+	{
+		$this->assertTrue($this->model->isValid());
+		$this->assertEmpty($this->model->errors());
+	}
+
+}
+
+class Nametastic
+{
+	public $data;
+
+	public function __construct(array $data)
+	{
+		$this->data = $data;
+	}
+
+	public function reverseName()
+	{
+		return strrev($this->data['name']);
+	}
+}
